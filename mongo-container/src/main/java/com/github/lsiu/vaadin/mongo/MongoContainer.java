b802206efca4b49324316d61d9173407af4213f2
lsiu <leonard.siu@gmail.com>
2013-07-29 01:26:07 +0800

    fix only shows 2 record instances

--- mongo-container/src/main/java/com/github/lsiu/vaadin/mongo/MongoContainer.java
+++ mongo-container/src/main/java/com/github/lsiu/vaadin/mongo/MongoContainer.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.bson.NewBSONDecoder;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -14,12 +16,13 @@ import com.vaadin.data.Property;
 import com.vaadin.data.util.AbstractContainer;
 
 @SuppressWarnings("serial")
-public class MongoContainer extends AbstractContainer implements Container.Ordered, Container.Sortable {
-	
+public class MongoContainer extends AbstractContainer implements
+		Container.Ordered, Container.Sortable {
+
 	private DBCollection collection;
-	
+
 	private String idField;
-	
+
 	public MongoContainer(DBCollection collection, String idField) {
 		this.collection = collection;
 		this.idField = idField;
@@ -27,7 +30,8 @@ public class MongoContainer extends AbstractContainer implements Container.Order
 
 	@Override
 	public Item getItem(Object itemId) {
-		DBObject dbObject = collection.findOne(new BasicDBObject(idField, itemId));
+		DBObject dbObject = collection.findOne(new BasicDBObject(idField,
+				itemId));
 		return new MongoItem(dbObject);
 	}
 
@@ -46,10 +50,11 @@ public class MongoContainer extends AbstractContainer implements Container.Order
 			filter = new BasicDBObject("_id", 0);
 			filter.put(idField, 1);
 		}
-		
-		List<DBObject> list = collection.find(new BasicDBObject(), filter).toArray();
+
+		List<DBObject> list = collection.find(new BasicDBObject(), filter)
+				.toArray();
 		List<Object> ids = new ArrayList<>();
-		for(int i=0; i < list.size(); i++) {
+		for (int i = 0; i < list.size(); i++) {
 			ids.add(list.get(i).get(idField));
 		}
 		return ids;
@@ -58,31 +63,35 @@ public class MongoContainer extends AbstractContainer implements Container.Order
 	@Override
 	@SuppressWarnings("rawtypes")
 	public Property getContainerProperty(Object itemId, Object propertyId) {
-		DBObject object = collection.findOne(new BasicDBObject(idField, itemId));
+		DBObject object = collection
+				.findOne(new BasicDBObject(idField, itemId));
 		if (object == null) {
 			return null;
 		} else {
-			return new MongoProperty<Object>(object, String.valueOf(propertyId), Object.class);
+			return new MongoProperty<Object>(object,
+					String.valueOf(propertyId), Object.class);
 		}
 	}
 
 	@Override
 	public Class<?> getType(Object propertyId) {
 		String key = String.valueOf(propertyId);
-		DBObject object = collection.findOne(new BasicDBObject(key, new BasicDBObject("$exists", true)));
+		DBObject object = collection.findOne(new BasicDBObject(key,
+				new BasicDBObject("$exists", true)));
 		Object field = object.get(String.valueOf(propertyId));
-		return field == null? Object.class : field.getClass();
+		return field == null ? Object.class : field.getClass();
 	}
 
 	@Override
 	public int size() {
 		long count = collection.getCount();
-		return count > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int)count;
+		return count > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) count;
 	}
 
 	@Override
 	public boolean containsId(Object itemId) {
-		return collection.findOne(new BasicDBObject(idField, itemId)) == null ? false : true;
+		return collection.findOne(new BasicDBObject(idField, itemId)) == null ? false
+				: true;
 	}
 
 	@Override
@@ -116,32 +125,64 @@ public class MongoContainer extends AbstractContainer implements Container.Order
 
 	@Override
 	public Object nextItemId(Object itemId) {
-		DBObject dbObject = collection.find(new BasicDBObject(this.idField, itemId)).sort(sortField).explain();
-		Integer n = (Integer)dbObject.get("n");
-		return collection.find().sort(sortField).skip(n).limit(1).next().get(idField);
+		DBCursor cur = collection
+				.find(new BasicDBObject(this.idField, new BasicDBObject("$gt",
+						itemId))).sort(sortField).limit(2);
+		if (cur.hasNext()) {
+			cur.next(); // this is the actual doc with itemId. We need the next
+						// one.
+			if (cur.hasNext()) {
+				return cur.next().get(idField);
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public Object prevItemId(Object itemId) {
-		DBObject dbObject = collection.find(new BasicDBObject(this.idField, itemId)).sort(sortField).explain();
-		Integer n = (Integer)dbObject.get("n");
-		return collection.find().sort(sortField).skip(n-2).limit(1).next().get(idField);
+		BasicDBObject negatedSortedField = new BasicDBObject();
+		for(String key: sortField.keySet()) {
+			int asc = (int)sortField.get(key);
+			negatedSortedField.put(key, asc);
+		}
+		
+		DBCursor cur = collection
+				.find(new BasicDBObject(this.idField, new BasicDBObject("$lt",
+						itemId))).sort(negatedSortedField).limit(2);
+
+		if (cur.hasNext()) {
+			cur.next(); // this is the actual doc with itemId. We need the next
+						// one.
+			if (cur.hasNext()) {
+				return cur.next().get(idField);
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public Object firstItemId() {
 		DBCursor cursor = collection.find().sort(sortField).limit(1);
-		return cursor.hasNext()? cursor.next().get(idField) : null;
+		return cursor.hasNext() ? cursor.next().get(idField) : null;
 	}
 
 	@Override
 	public Object lastItemId() {
 		long count = collection.count();
 		if (count > Integer.MAX_VALUE) {
-			throw new IllegalStateException("Collection size greater than Integer.MAX_VALUE (" + Integer.MAX_VALUE + ")");
+			throw new IllegalStateException(
+					"Collection size greater than Integer.MAX_VALUE ("
+							+ Integer.MAX_VALUE + ")");
 		}
-		int n = (int)count;
-		DBCursor cursor = collection.find().skip(n - 1).limit(1).sort(sortField); 
+		int n = (int) count;
+		DBCursor cursor = collection.find().skip(n - 1).limit(1)
+				.sort(sortField);
 		return cursor.next().get(idField);
 	}
 
@@ -160,10 +201,13 @@ public class MongoContainer extends AbstractContainer implements Container.Order
 	public boolean isLastId(Object itemId) {
 		long count = collection.count();
 		if (count > Integer.MAX_VALUE) {
-			throw new IllegalStateException("Collection size greater than Integer.MAX_VALUE (" + Integer.MAX_VALUE + ")");
+			throw new IllegalStateException(
+					"Collection size greater than Integer.MAX_VALUE ("
+							+ Integer.MAX_VALUE + ")");
 		}
-		int n = (int)count;
-		DBCursor cursor = collection.find().skip(n - 1).limit(1).sort(sortField);
+		int n = (int) count;
+		DBCursor cursor = collection.find().skip(n - 1).limit(1)
+				.sort(sortField);
 		DBObject dbObject = null;
 		if (cursor.hasNext()) {
 			dbObject = cursor.next();
@@ -183,15 +227,15 @@ public class MongoContainer extends AbstractContainer implements Container.Order
 			throws UnsupportedOperationException {
 		throw new UnsupportedOperationException();
 	}
-	
-	private BasicDBObject sortField;
+
+	private BasicDBObject sortField = new BasicDBObject("_id", 1);
 
 	@Override
 	public void sort(Object[] propertyId, boolean[] ascending) {
 		sortField = new BasicDBObject();
 		if (propertyId != null) {
-			
-			for(int i = 0; i < propertyId.length; i++) {
+
+			for (int i = 0; i < propertyId.length; i++) {
 				boolean asc = false;
 				if (ascending != null && i < ascending.length) {
 					asc = ascending[i];
@@ -204,9 +248,9 @@ public class MongoContainer extends AbstractContainer implements Container.Order
 	@Override
 	public Collection<?> getSortableContainerPropertyIds() {
 		List<DBObject> indexes = collection.getIndexInfo();
-		List<Object> indexedPropertyIds = new ArrayList<>(); 
-		for (DBObject i: indexes) {
-			DBObject dbObject = (DBObject)i.get("key");
+		List<Object> indexedPropertyIds = new ArrayList<>();
+		for (DBObject i : indexes) {
+			DBObject dbObject = (DBObject) i.get("key");
 			Object key = dbObject.keySet().iterator().next();
 			indexedPropertyIds.add(key);
 		}
